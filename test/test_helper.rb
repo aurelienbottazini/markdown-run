0f38dc7caffc33ed0e98de469ee92ead78c7745d
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2025-06-06 17:24:01 +0200

    remove minimum coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,16 +6,6 @@ SimpleCov.start do
   add_filter '/test/'
   add_filter '/vendor/'
 
-  # Set minimum coverage threshold (optional)
-  minimum_coverage 75
-
-  # Track branches as well as lines (more comprehensive coverage)
-  enable_coverage :branch
-
-  # Custom coverage groups (optional)
-  add_group "Core Library", "lib"
-  add_group "Executors", "lib/*_executor.rb"
-  add_group "Processors", "lib/*_processor.rb"
 end
 
 require 'minitest/autorun'
